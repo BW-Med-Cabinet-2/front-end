1223e87abd164d01304f9d4b91c470774be4ce7e
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-08-24 17:25:27 -0500

    basic login form

--- medi-cabinet/src/App.js
+++ medi-cabinet/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import RegisterForm from './Components/Register/RegisterForm.js';
+import Login from './Components/Login/Login.js';
+
 
 import './App.css';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <RegisterForm />
+      <Login />
     </div>
   );
 }
